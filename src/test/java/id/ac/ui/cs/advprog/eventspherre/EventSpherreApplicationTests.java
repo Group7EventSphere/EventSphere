bd46c22856901065156bae6f385c2d0b556446cb
davinawuy <alano.davin@office.ui.ac.id>
2025-03-25 13:28:33 +0700

    [feat] Initial Commit

---
+++ src/test/java/id/ac/ui/cs/advprog/eventspherre/EventSpherreApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eventspherre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSpherreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
