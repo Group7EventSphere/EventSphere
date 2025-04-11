1de8ecc20c586ccb6d2fa5e6cae3da8b6d7e0a96
davinawuy <alano.davin@office.ui.ac.id>
2025-04-11 17:03:05 +0700

    [GREEN] Implementing SufficientBalanceHandler for component of Chain of Responsibility

---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/handler/SufficientBalanceHandler.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.eventspherre.handler;
+
+import id.ac.ui.cs.advprog.eventspherre.model.User;
+import id.ac.ui.cs.advprog.eventspherre.model.PaymentRequest;
+
+public class SufficientBalanceHandler implements PaymentHandler {
+    private PaymentHandler next;
+
+    @Override
+    public void setNext(PaymentHandler handler) {
+        this.next = handler;
+    }
+
+    @Override
+    public void handle(PaymentRequest request) {
+        if (request.getPaymentType() == PaymentRequest.PaymentType.PURCHASE) {
+            User user = request.getUser();
+            if (user.getBalance() < request.getAmount()) {
+                request.setProcessed(true);
+                request.setMessage("Payment failed: insufficient funds");
+                return; // Stop the chain if funds are insufficient.
+            }
+        }
+        if (next != null) {
+            next.handle(request);
+        }
+    }
+}
