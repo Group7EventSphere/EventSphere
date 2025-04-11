0e50c0f9dee232ffdfbf084b1bf040b916cc4e83
rayienda <rayienda.hasmaradana@ui.ac.id>
2025-04-11 15:55:40 +0700

    [GREEN] Implement Ads Model

---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/model/Ad.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.eventspherre.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Ad {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+
+    public Ad() { }
+
+    public Ad(Long id, String title, String description, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+}
