fa8517643b21f84534b5454760190bd7cbc3a364
DamarAR <aryaputra.damar@gmail.com>
2025-05-07 12:54:00 +0700

    [Refactor] Implement User

--- src/main/java/id/ac/ui/cs/advprog/eventspherre/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/model/User.java
@@ -2,21 +2,44 @@ package id.ac.ui.cs.advprog.eventspherre.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import jakarta.persistence.*;
 import java.util.Date;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+@Entity
+@Table (name = "users")
 @Getter
 @Setter
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private Integer id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true, length = 100, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
     private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
     private Date updatedAt;
-    private Role role;
-    private Double balance;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role= Role.ATTENDEE;
+
+    @Column(name = "balance")
+    private Double balance= 0.0;
 
     public enum Role {
         ADMIN, ORGANIZER, ATTENDEE
