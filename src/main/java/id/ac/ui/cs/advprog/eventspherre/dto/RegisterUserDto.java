e04d540fc2b538f221c914d4ea04cec5d0fec5b0
DamarAR <aryaputra.damar@gmail.com>
2025-05-08 13:30:49 +0700

    [GREEN] implement register user dto

---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/dto/RegisterUserDto.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eventspherre.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RegisterUserDto {
+    private String email;
+    
+    private String password;
+    
+    private String name;
+}
