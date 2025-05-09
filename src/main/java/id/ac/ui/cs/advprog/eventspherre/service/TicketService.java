da21359a367f058970359cb0cc570fa69970641b
wuyu0107 <mkim050107@gmail.com>
2025-05-09 13:19:17 +0700

    [RED] implement skeleton for service

---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/service/TicketService.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eventspherre.service;
+
+import id.ac.ui.cs.advprog.eventspherre.model.Ticket;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TicketService {
+
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/service/TicketServiceImpl.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.eventspherre.service;
+
+import id.ac.ui.cs.advprog.eventspherre.repository.TicketRepository;
+import id.ac.ui.cs.advprog.eventspherre.service.TicketService;
+
+public class TicketServiceImpl implements TicketService {
+    private TicketRepository ticketRepository;
+}
