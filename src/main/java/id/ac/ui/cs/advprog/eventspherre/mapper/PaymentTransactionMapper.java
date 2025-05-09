5b09fe092f749d76f39a27b4405b13644199fc44
davinawuy <alano.davin@office.ui.ac.id>
2025-05-09 17:56:25 +0700

    [GREEN] Implement PaymentTransactionMapper

---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/mapper/PaymentTransactionMapper.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.eventspherre.mapper;
+
+import id.ac.ui.cs.advprog.eventspherre.dto.PaymentTransactionDTO;
+import id.ac.ui.cs.advprog.eventspherre.model.PaymentTransaction;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class PaymentTransactionMapper {
+    public PaymentTransactionDTO toDto(PaymentTransaction tx) {
+        return new PaymentTransactionDTO(
+                tx.getId(),
+                tx.getUserId(),
+                tx.getAmount(),
+                tx.getType(),
+                tx.getStatus(),
+                tx.getCreatedAt()
+        );
+    }
+
+    public List<PaymentTransactionDTO> toDtoList(List<PaymentTransaction> list) {
+        return list.stream().map(this::toDto).collect(Collectors.toList());
+    }
+}
