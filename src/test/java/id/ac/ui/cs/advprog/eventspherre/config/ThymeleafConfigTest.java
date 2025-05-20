9dbff1c5e4e7c2efbd5ba538de4fd90dd19782f6
DamarAR <aryaputra.damar@gmail.com>
2025-05-20 20:05:36 +0700

    [RED] src/test/java/id/ac/ui/cs/advprog/eventspherre/config/ThymeleafConfigTest.java

---
+++ src/test/java/id/ac/ui/cs/advprog/eventspherre/config/ThymeleafConfigTest.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.eventspherre.config;
+
+import org.junit.jupiter.api.Test;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ThymeleafConfigTest {
+
+    @Test
+    void springSecurityDialect_shouldReturnSpringSecurityDialectInstance() {
+        // Arrange
+        ThymeleafConfig config = new ThymeleafConfig();
+        
+        // Act
+        SpringSecurityDialect dialect = config.springSecurityDialect();
+        
+        // Assert
+        assertNotNull(dialect);
+        assertThat(dialect).isInstanceOf(SpringSecurityDialect.class);
+    }
+    
+    @Test
+    void springSecurityDialect_shouldHaveCorrectDialectPrefix() {
+        // Arrange
+        ThymeleafConfig config = new ThymeleafConfig();
+        
+        // Act
+        SpringSecurityDialect dialect = config.springSecurityDialect();
+        
+        // Assert
+        assertThat(dialect.getPrefix()).isEqualTo("sec");
+    }
+}
