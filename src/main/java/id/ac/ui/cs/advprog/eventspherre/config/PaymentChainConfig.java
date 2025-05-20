761cc74b8011b2580c0fcba36f02606ef1dc0f4d
davinawuy <alano.davin@office.ui.ac.id>
2025-05-20 17:41:14 +0700

    [REFACTOR] Added Concurrency to Chain design pattern so that payments can be done concurrently

--- src/main/java/id/ac/ui/cs/advprog/eventspherre/config/PaymentChainConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/config/PaymentChainConfig.java
@@ -4,21 +4,40 @@ import id.ac.ui.cs.advprog.eventspherre.handler.DeductBalanceHandler;
 import id.ac.ui.cs.advprog.eventspherre.handler.PaymentHandler;
 import id.ac.ui.cs.advprog.eventspherre.handler.SufficientBalanceHandler;
 import id.ac.ui.cs.advprog.eventspherre.handler.TopUpHandler;
+import id.ac.ui.cs.advprog.eventspherre.model.PaymentRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class PaymentChainConfig {
 
     @Bean
-    public PaymentHandler paymentHandlerChain() {
-        SufficientBalanceHandler sufficientBalance = new SufficientBalanceHandler();
-        DeductBalanceHandler deductBalance = new DeductBalanceHandler();
-        TopUpHandler topUp = new TopUpHandler();
+    public ExecutorService paymentExecutor() {
+        return Executors.newFixedThreadPool(10);
+    }
 
-        sufficientBalance.setNext(deductBalance);
-        deductBalance.setNext(topUp);
+    @Bean
+    public PaymentHandler paymentHandlerChain(ExecutorService exec) {
+        SufficientBalanceHandler sufficient = new SufficientBalanceHandler();
+        DeductBalanceHandler deduct         = new DeductBalanceHandler();
+        TopUpHandler topUp                  = new TopUpHandler();
 
-        return sufficientBalance;
+        sufficient.setNext(deduct);
+        deduct.setNext(topUp);
+
+        return new PaymentHandler() {
+            @Override
+            public void setNext(PaymentHandler handler) {
+                sufficient.setNext(handler);
+            }
+
+            @Override
+            public void handle(PaymentRequest request) {
+                exec.submit(() -> sufficient.handle(request));
+            }
+        };
     }
 }
