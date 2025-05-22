47eb41b708798f4252982442e331e4b206160aff
adyoprawira <adyoprawira@gmail.com>
2025-05-23 01:02:41 +0700

    [RED] Adding Event Repository Skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.eventspherre.repository;
+
+import id.ac.ui.cs.advprog.eventspherre.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
