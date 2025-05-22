7b5fb6c0ff7814ad6d7e40c833da321dced82d40
syifa-ananda <syifaananda36@gmail.com>
2025-05-22 21:49:01 +0700

    [REFACTOR] Update Review model and controller for event integration

--- src/main/java/id/ac/ui/cs/advprog/eventspherre/model/Review.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/model/Review.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Entity
 @Table(name="reviews")
 @Getter
@@ -12,17 +14,21 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "event_id", columnDefinition = "uuid")
     private Long eventId;
+
     private Long attendeeId;
     private String reviewText;
     private int rating;
 
     public Review() { }
 
-    public Review(Long eventId, Long attendeeId, String reviewText, int rating) {
+    public Review(long eventId, Long attendeeId, String reviewText, int rating) {
         this.eventId = eventId;
         this.attendeeId = attendeeId;
         this.reviewText = reviewText;
         this.rating = rating;
     }
 }
+
