f2a14e340e06f200cfc85d5f2de32155067cd261
adyoprawira <adyoprawira@gmail.com>
2025-05-23 16:41:12 +0700

    [REFACTOR] Fixed ID to use Int instead of UUID

--- src/main/java/id/ac/ui/cs/advprog/eventspherre/repository/EventRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/repository/EventRepository.java
@@ -3,7 +3,5 @@ package id.ac.ui.cs.advprog.eventspherre.repository;
 import id.ac.ui.cs.advprog.eventspherre.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface EventRepository extends JpaRepository<Event, Integer> {
 }
