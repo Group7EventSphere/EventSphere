9fa2747b23afa1b2063c55c814948dbfa090cca7
rayienda <rayienda.hasmaradana@ui.ac.id>
2025-05-24 21:01:04 +0700

    Finishing Ads Management

--- src/main/java/id/ac/ui/cs/advprog/eventspherre/controller/AdController.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/controller/AdController.java
@@ -1,21 +1,12 @@
 package id.ac.ui.cs.advprog.eventspherre.controller;
 
-import id.ac.ui.cs.advprog.eventspherre.dto.AdRequestDto;
-import id.ac.ui.cs.advprog.eventspherre.model.Ad;
 import id.ac.ui.cs.advprog.eventspherre.service.AdService;
 import id.ac.ui.cs.advprog.eventspherre.service.ImageStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.stereotype.Controller;
-
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/ads")
@@ -32,13 +23,23 @@ public class AdController {
     }
 
     @GetMapping("/create")
+    @PreAuthorize("hasAnyRole('ORGANIZER','ADMIN')")
     public String createAds(Model model) {
-        return "create-ads";
+        return "ads/create-ads";
+    }
+
+    @GetMapping("/list")
+    @PreAuthorize("hasAnyRole('ORGANIZER','ADMIN')")
+    public String listAds(Model model) {
+        model.addAttribute("ads", adService.getAllAds());
+        return "ads/listads";
     }
 
     @GetMapping("/edit/{id}")
-    public String editAds(@PathVariable("id") Long id, Model model) {
+    @PreAuthorize("hasAnyRole('ORGANIZER','ADMIN')")
+    public String editAds (@PathVariable("id") Long id, Model model){
         model.addAttribute("id", id);
-        return "EditAds";
+        model.addAttribute("ad", adService.getAd(id));
+        return "ads/EditAds";
     }
 }
