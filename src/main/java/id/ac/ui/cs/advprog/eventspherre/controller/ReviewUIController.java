d6a27141bc20317424ee3dd91922f4cb3146c2c3
syifa-ananda <syifaananda36@gmail.com>
2025-05-24 16:13:17 +0700

    [REFACTOR] Include review to event detail page

--- src/main/java/id/ac/ui/cs/advprog/eventspherre/controller/ReviewUIController.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/controller/ReviewUIController.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eventspherre.controller;
 
-import id.ac.ui.cs.advprog.eventspherre.model.Review;
 import id.ac.ui.cs.advprog.eventspherre.repository.ReviewRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
