f3fdacc2e162da4733273574ea8179ed88e7f7dc
davinawuy <alano.davin@office.ui.ac.id>
2025-05-24 17:51:21 +0700

    [REFACTOR] Fixing ReviewRepositoryTest

--- src/test/java/id/ac/ui/cs/advprog/eventspherre/repository/ReviewRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eventspherre/repository/ReviewRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class ReviewRepositoryTest {
 
     @Autowired
