25b1a1b925ff40eb228dd8342f2bf92bc75d44c4
rayienda <rayienda.hasmaradana@ui.ac.id>
2025-05-25 16:11:39 +0700

    [REFACTOR] Updated AdTest

--- src/test/java/id/ac/ui/cs/advprog/eventspherre/model/AdTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eventspherre/model/AdTest.java
@@ -44,4 +44,72 @@ class AdTest {
 
         assertNull(adWithoutImage.getImageUrl());
     }
+
+    @Test
+    void testAllArgsConstructorWithFullParameters() {
+        Ad ad = new Ad(
+                1L,
+                "Ad Title",
+                "Ad Description",
+                "http://image.url",
+                "ADMIN",
+                false
+        );
+
+        assertEquals(1L, ad.getId());
+        assertEquals("Ad Title", ad.getTitle());
+        assertEquals("Ad Description", ad.getDescription());
+        assertEquals("http://image.url", ad.getImageUrl());
+        assertEquals("ADMIN", ad.getUserRole());
+        assertFalse(ad.isActive());
+    }
+
+    @Test
+    void testConstructorWithUserRoleOnly() {
+        Ad ad = new Ad(
+                2L,
+                "Second Ad",
+                "Description",
+                "http://image2.url",
+                "ORGANIZER"
+        );
+
+        assertEquals(2L, ad.getId());
+        assertEquals("Second Ad", ad.getTitle());
+        assertEquals("Description", ad.getDescription());
+        assertEquals("http://image2.url", ad.getImageUrl());
+        assertEquals("ORGANIZER", ad.getUserRole());
+        assertTrue(ad.isActive()); // karena default true
+    }
+
+    @Test
+    void testConstructorWithDefaults() {
+        Ad ad = new Ad(
+                3L,
+                "Default Ad",
+                "Default Description",
+                "http://image3.url"
+        );
+
+        assertEquals("USER", ad.getUserRole());
+        assertTrue(ad.isActive());
+    }
+
+    @Test
+    void testSettersAndGetters() {
+        Ad ad = new Ad();
+        ad.setId(4L);
+        ad.setTitle("Title");
+        ad.setDescription("Desc");
+        ad.setImageUrl("url");
+        ad.setUserRole("MODERATOR");
+        ad.setActive(false);
+
+        assertEquals(4L, ad.getId());
+        assertEquals("Title", ad.getTitle());
+        assertEquals("Desc", ad.getDescription());
+        assertEquals("url", ad.getImageUrl());
+        assertEquals("MODERATOR", ad.getUserRole());
+        assertFalse(ad.isActive());
+    }
 }
