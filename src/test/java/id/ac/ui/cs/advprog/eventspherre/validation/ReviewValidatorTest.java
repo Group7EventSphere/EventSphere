418474b2403882856dedf654ad94277261460914
syifa-ananda <syifaananda36@gmail.com>
2025-05-25 14:56:11 +0700

    [REFACTOR] Remove public modifiers

--- src/test/java/id/ac/ui/cs/advprog/eventspherre/validation/ReviewValidatorTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eventspherre/validation/ReviewValidatorTest.java
@@ -4,25 +4,28 @@ import id.ac.ui.cs.advprog.eventspherre.model.Review;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ReviewValidatorTest {
+class ReviewValidatorTest {
 
     @Test
-    public void testValidReview() {
+    void testValidReview() {
         Review review = new Review(1, 101L, "Excellent event!", 5);
         DefaultReviewValidator validator = new DefaultReviewValidator();
         assertTrue(validator.isValid(review));
     }
 
     @Test
-    public void testInvalidReviewEmptyText() {
+    void testInvalidReviewEmptyText() {
         Review review = new Review(1, 101L, "", 5);
         DefaultReviewValidator validator = new DefaultReviewValidator();
         assertFalse(validator.isValid(review));
-        assertEquals("Invalid review: Review text cannot be empty and rating must be between 1 and 5.", validator.getErrorMessage());
+        assertEquals(
+                "Invalid review: Review text cannot be empty and rating must be between 1 and 5.",
+                validator.getErrorMessage()
+        );
     }
 
     @Test
-    public void testInvalidReviewRatingOutOfRange() {
+    void testInvalidReviewRatingOutOfRange() {
         Review review = new Review(1, 101L, "Not bad", 8);
         DefaultReviewValidator validator = new DefaultReviewValidator();
         assertFalse(validator.isValid(review));
