c27d61724353856ed9a618936f3fe52665fb993d
davinawuy <alano.davin@office.ui.ac.id>
2025-05-26 22:23:15 +0700

    [REFACTOR] Improving Coverage on Handlers and Payment Chain

--- src/test/java/id/ac/ui/cs/advprog/eventspherre/EventSpherreApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eventspherre/EventSpherreApplicationTests.java
@@ -1,6 +1,10 @@
 package id.ac.ui.cs.advprog.eventspherre;
 
+import static org.mockito.Mockito.mockStatic;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +12,17 @@ class EventSpherreApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testMainMethod() {
+        try (MockedStatic<SpringApplication> mockedSpringApplication = mockStatic(SpringApplication.class)) {
+            String[] args = {"arg1", "arg2"};
+            
+            EventSpherreApplication.main(args);
+            
+            mockedSpringApplication.verify(() -> SpringApplication.run(EventSpherreApplication.class, args));
+        }
     }
 
 }
