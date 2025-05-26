97b6eab5d732770bce1ca3645e3a5da37967c868
rayienda <rayienda.hasmaradana@ui.ac.id>
2025-05-26 19:04:44 +0700

    [REFACTOR] Fix Sonarcloud

--- src/main/java/id/ac/ui/cs/advprog/eventspherre/service/AdService.java
+++ src/main/java/id/ac/ui/cs/advprog/eventspherre/service/AdService.java
@@ -8,6 +8,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,7 +37,9 @@ public class AdService {
                 .block();
 
         if (response == null) {
-            throw new RuntimeException("No response from /api/ads");
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "No response body from /api/ads");
         }
         return response;
     }
