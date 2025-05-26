6c90b4b7874c54d2733b7f1efa977510ab8b1556
rayienda <rayienda.hasmaradana@ui.ac.id>
2025-05-26 19:45:00 +0700

    [REFACTOR] Fix Sonarcloud-service

--- src/test/java/id/ac/ui/cs/advprog/eventspherre/service/FileSystemImageStorageServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eventspherre/service/FileSystemImageStorageServiceTest.java
@@ -7,7 +7,6 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
